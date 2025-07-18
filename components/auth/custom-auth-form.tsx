2587dbd87d4a9827affcd151f03824c2de078f64
std-1224 <kalkaudy@gmail.com>
2025-07-18 11:47:37 +0700

    add call back

--- components/auth/custom-auth-form.tsx
+++ components/auth/custom-auth-form.tsx
@@ -149,7 +149,7 @@ export function CustomAuthForm() {
     setMessage(null)
 
     try {
-      await signInWithOAuth('google', `${process.env.NEXT_PUBLIC_REDIRECT_URL}`)
+      await signInWithOAuth('google', `${process.env.NEXT_PUBLIC_REDIRECT_URL}/auth/callback`)
       toast({
         title: "Redirigiendo a Google",
         description: "Serás redirigido para autenticarte con Google",
